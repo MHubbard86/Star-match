a98603221230c6cc1e0e37ac0a04bb67f0b47481
Matthew Hubbard <mjhubb@hotmail.co.uk>
2020-11-02 15:20:48 +0000

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Game from './Game'
+
+// STAR MATCH - V9
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
+export function App() {
+ return <StarMatch />
+}
